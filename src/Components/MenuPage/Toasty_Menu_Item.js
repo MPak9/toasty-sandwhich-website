0d4e8adeaff06e1713fc3265557ee8137f3184cc
MPak9 <56807392+MPak9@users.noreply.github.com>
2023-09-11 19:28:35 -0400

    Menu Item Component finsihed
    Need to fill it in later with JSON data
    Note: Should a slider be added?

--- src/Components/MenuPage/Toasty_Menu_Item.js
+++ src/Components/MenuPage/Toasty_Menu_Item.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const Toasty_Menu_Item = () => {
+const Toasty_Menu_Item = ({name, price, imgSrc}) => {
   return (
-    <div className='flex-container'>
-        <div style={{display:'flex', zIndex:'-2', justifyContent:'center'}}>
+    <div className='menu_item_component_grid'>
+        <div style={{ gridRow:'1/ span 2', gridColumn:'1/ span 2', display:'flex', zIndex:'-2', justifyContent:'center'}}>
                <img className='sandwich_of_month' alt='sandwich' src='https://cdn.discordapp.com/attachments/1075543412582924378/1088138105875341453/pngegg_8.png'/>
         </div>
-        <p style={{textAlign:'center'}}>
-            Name
+        <p className='comment stroke_alice' style={{gridRow:'2', gridColumn:'2', textAlign:'center'}}>
+            {price}
+        </p>
+        <p style={{gridRow:'3', gridColumn:'1/ span 2', textAlign:'center'}}>
+            {name}
         </p>
     </div>
   )
--- src/Components/MenuPage/Toasty_Original_Menu.js
+++ src/Components/MenuPage/Toasty_Original_Menu.js
@@ -6,16 +6,16 @@ const Toasty_Original_Menu = () => {
     const {width} = useWindowSize();
     const minWidth = 600;
 
-    const original = [1,2,3,4,5,6,7,8,9,10];
+    const original = ["1.00","2.00","3.00","4.00","5.00","6.00","7.00","8.00","9.00","10.00"];
 
   return (
     <div className='menu_original_grid'>
         <h2 style={{ gridRow: '2', gridColumn: '2'}}>
             Toasty Original Sandwiches
         </h2>
-        <div style={{gridRow:'2', gridColumn:'2'}}>
+        <div style={{gridRow:'4', gridColumn:'2', display:'grid', gridTemplateColumns: width > minWidth ? 'repeat(5, 1fr)': 'repeat(3, 1fr)'}}>
             {
-                original.map(({color, hex}) => <Toasty_Menu_Item/>)
+                original.map((i) => <Toasty_Menu_Item name={i} price={i} />)
             }
             
         </div>
