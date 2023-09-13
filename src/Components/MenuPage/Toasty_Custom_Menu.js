3fc04d7d706ed83976cf7864aaf66dbbc40dece3
MPak9 <56807392+MPak9@users.noreply.github.com>
2023-09-13 19:43:16 -0400

    Working on Custom Make Menu

--- src/Components/MenuPage/Toasty_Custom_Menu.js
+++ src/Components/MenuPage/Toasty_Custom_Menu.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import Toasty_Menu_Item from './Toasty_Menu_Item';
+import useWindowSize from '../../Hooks/useWindowSize'
 
 const Toasty_Custom_Menu = () => {
+    const {width} = useWindowSize();
+    const minWidth = 600;
+
+    const original = ["1.00","2.00","3.00","4.00","5.00","6.00","7.00","8.00"];
+
   return (
     <div className='menu_custom_grid'>
         <h2 style={{ gridRow: '2', gridColumn: '2 / span 3'}}>
             Custom Make!
         </h2>
+        <h3 style={{ gridRow: '3', gridColumn: '2 / span 3'}}>
+            Breads
+        </h3>
+        <div style={{gridRow:'4', gridColumn:'2 / span 5', display:'grid', gridTemplateColumns: width > minWidth ? 'repeat(5, 1fr)': 'repeat(3, 1fr)'}}>
+            {
+                original.map((i) => <Toasty_Menu_Item name={i} price={i} />)
+            }
+            
+        </div>
+        <h3 style={{ gridRow: '5', gridColumn: '2 / span 3'}}>
+            Fillings
+        </h3>
+        <h3 style={{ gridRow: '5', gridColumn: '4'}}>
+            Toppings
+        </h3>
+        <h3 style={{ gridRow: '5', gridColumn: '5'}}>
+            Cheeses
+        </h3>
+        <h3 style={{ gridRow: '5', gridColumn: '6'}}>
+            Sauces
+        </h3>
         <div style={{ gridRow: '1 / span 10', gridColumn: '1 / span 7', zIndex:'-4', backgroundColor:'wheat'}}></div>
     </div>
   )
