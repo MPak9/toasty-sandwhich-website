a7ae91a35daaed0de2eaea8bf35cc817329947e4
MPak9 <56807392+MPak9@users.noreply.github.com>
2023-09-14 13:31:56 -0400

    Nearly Done with Custom Menu Section
    Needs to be cleaned up for Mobile Responsiveness

--- src/Components/MenuPage/Toasty_Custom_Menu.js
+++ src/Components/MenuPage/Toasty_Custom_Menu.js
@@ -22,38 +22,55 @@ const Toasty_Custom_Menu = () => {
             }
             
         </div>
-        <h3 style={{ gridRow: '5', gridColumn: '2'}}>
+        <h3 style={{ gridRow: '6', gridColumn: '2'}}>
             Fillings
         </h3>
-        <ul style={{ gridRow: '6', gridColumn: '2'}}>
+        <ul style={{ gridRow: '7', gridColumn: width > 600 ? '2' : '2/ span 3'}}>
             {
                 original.map((i) => <li className='normal'><span>{i}</span> <span style={{paddingLeft:'5vw'}} >{i}</span></li>)
             }
         </ul>
-        <h3 style={{ gridRow: '5', gridColumn: '4'}}>
+        <h3 style={{ gridRow: '6', gridColumn: width > 600 ? '4' : '5'}}>
             Toppings
         </h3>
-        <ul style={{ gridRow: '6', gridColumn: '4'}}>
+        <ul style={{ gridRow: '7', gridColumn: width > 600 ? '4' : '5'}}>
             {
                 original.map((i) => <li className='normal'><span>{i}</span></li>)
             }
         </ul>
-        <h3 style={{ gridRow: '5', gridColumn: '5'}}>
+        <h3 style={{ gridRow: width > 600 ? '6' : '8', gridColumn: width > 600 ? '5' : '2'}}>
             Cheeses
         </h3>
-        <ul style={{ gridRow: '6', gridColumn: '5'}}>
+        <ul style={{ gridRow: width > 600 ? '7' : '9', gridColumn: width > 600 ? '5' : '2'}}>
             {
                 original.map((i) => <li className='normal'><span>{i}</span></li>)
             }
         </ul>
-        <h3 style={{ gridRow: '5', gridColumn: '6'}}>
+        <h3 style={{ gridRow: width > 600 ? '6' : '8', gridColumn: width > 600 ? '6' : '5'}}>
             Sauces
         </h3>
-        <ul style={{ gridRow: '6', gridColumn: '6'}}>
+        <ul style={{ gridRow: width > 600 ? '7' : '9', gridColumn: width > 600 ? '6' : '5'}}>
             {
                 original.map((i) => <li className='normal'><span>{i}</span></li>)
             }
         </ul>
+
+        <div style={{
+            display:'grid',
+            gridTemplateRows:'1fr 0.1fr',
+            gridColumn:'1 / span 7',
+            gridRow: '8',
+            marginBottom:'0',
+            zIndex:'-3'
+        }}>
+            <div style={{ display:'flex', flexDirection:'column-reverse', gridColumn:'1' ,gridRow:'1 / span 2', verticalAlign:'bottom'}}>
+                <svg preserveAspectRatio='none' viewBox="0 0 1440 182" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 137.781C744 227.036 673 -47.5946 1440 7.33154V182H0V137.781Z" fill="#F0F8FF"/>
+                </svg>
+            </div>
+            <div style={{ gridColumn:'1', gridRow:'2', backgroundColor:'aliceBlue'}}></div>
+        </div>
+        <div style={{ gridRow: '9 / span 5', gridColumn: '1 / span 7', zIndex:'-3', backgroundColor:'aliceBlue'}}></div>
         <div style={{ gridRow: '1 / span 10', gridColumn: '1 / span 7', zIndex:'-4', backgroundColor:'wheat'}}></div>
     </div>
   )
