56db53eee4f8e3f2ce4717643ef2e3e56479d80e
MPak9 <56807392+MPak9@users.noreply.github.com>
2023-09-21 14:05:33 -0400

    Updating Bread.js - Updating images in Custom menu

--- src/JSON API/Breads.js
+++ src/JSON API/Breads.js
@@ -2,18 +2,23 @@ import React from "react";
 
 export const Breads = [
     {
-        name:'Subroll'
+        name:'Subroll',
+        img:'https://cdn.discordapp.com/attachments/1075543412582924378/1154110115708940319/subroll.png'
     },
     {
-        name:'Sour Dough'
+        name:'Sour Dough',
+        img:'https://cdn.discordapp.com/attachments/1075543412582924378/1154112094858383462/sourdoughbreadloaf.png'
     },
     {
-        name:'Whole Wheat'
+        name:'Whole Wheat',
+        img:'https://cdn.discordapp.com/attachments/1075543412582924378/1154115305187049512/wholewheat.png'
     },
     {
-        name:'Milk Bread'
+        name:'Milk Bread',
+        img:'https://cdn.discordapp.com/attachments/1075543412582924378/1154120083107487854/milkbread.png'
     },
     {
-        name:'English Muffin'
+        name:'English Muffin',
+        img:'https://cdn.discordapp.com/attachments/1075543412582924378/1154124561160343693/englishmuffin.png'
     },
 ]
